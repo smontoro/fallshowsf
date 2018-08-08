c658bf18129493e5177b6a6d111740b71f8077c3
smontoro <sarahjmontoro@gmail.com>
2018-08-08 10:24:38 -0700

    added styles and organized css

--- page-about.php
+++ page-about.php
@@ -38,78 +38,85 @@ while (have_posts()) :
     ?>
 
     <section class="container page-margin" id="about">
+        <div id="aboutPrimary">
             <header class="section-header">
                 <h2><?php the_title() ?></h2>
             </header>
             <?php the_content(); ?>
-
-        <div class="container about-chairs">
-            <div>
-                <h5><?php echo $chair_year; ?></h5>
-                <hr>
-            </div>  
-            <div>
-                <h6><?php echo $chair_1; ?></h6>
-                <p><?php echo $chair_1_name; ?></p>
-            </div> 
-            <div>
-                <h6><?php echo $chair_2; ?></h6>
-                <p><?php echo $chair_2_name; ?></p>
-            </div>   
-            <div>
-                <h6><?php echo $chair_3; ?></h6>
-                <p><?php echo $chair_3_name; ?></p>
-            </div> 
-            <div>
-                <h6><?php echo $chair_4; ?></h6>
-                <p><?php echo $chair_4_name; ?></p>
-            </div> 
-            <div>
-                <h6><?php echo $chair_5; ?></h6>
-                <p><?php echo $chair_5_name; ?></p>
-            </div> 
         </div>
+            
 
-        <div class="container about-gala">
-            <div>
-                <h5><?php echo $gala_committee_year; ?></h5>
-                <hr>
-                <div><?php echo $gala_committee_names_left; ?></div>
-                <div><?php echo $gala_committee_names_right; ?></div>
-            </div>
-        </div>
+            <div class="container" id="aboutSecondary">
 
-        <div class="container about-founders">
-            <div>
-                <h5><?php echo $founders_title; ?></h5>
-                <hr>
-                <div><?php echo $founder_names; ?></div>
-            </div>
-        </div>
+                <div class="row about-chairs">
+                    <div>
+                        <h5><?php echo $chair_year; ?></h5>
+                        <hr>
+                    </div>  
+                    <div>
+                        <h6><?php echo $chair_1; ?></h6>
+                        <p><?php echo $chair_1_name; ?></p>
+                    </div> 
+                    <div>
+                        <h6><?php echo $chair_2; ?></h6>
+                        <p><?php echo $chair_2_name; ?></p>
+                    </div>   
+                    <div>
+                        <h6><?php echo $chair_3; ?></h6>
+                        <p><?php echo $chair_3_name; ?></p>
+                    </div> 
+                    <div>
+                        <h6><?php echo $chair_4; ?></h6>
+                        <p><?php echo $chair_4_name; ?></p>
+                    </div> 
+                    <div>
+                        <h6><?php echo $chair_5; ?></h6>
+                        <p><?php echo $chair_5_name; ?></p>
+                    </div> 
+                </div>
 
-        <div class="container about-past-show-chairs">
-            <div>
-                <h5><?php echo $past_show_chairs_title; ?></h5>
-                <hr>
-                <div><?php echo $past_show_chairs_names; ?></div>
-            </div>
-        </div>
+                <div class="row about-gala">
+                    <div>
+                        <h5><?php echo $gala_committee_year; ?></h5>
+                        <hr>
+                        <div><?php echo $gala_committee_names_left; ?></div>
+                        <div><?php echo $gala_committee_names_right; ?></div>
+                    </div>
+                </div>
 
-        <div class="container about-staff">
-            <div>
-                <h5><?php echo $staff_title; ?></h5>
-                <hr>
-                <div><?php echo $staff_info; ?></div>
-            </div>
-        </div>
+                <div class="row about-founders">
+                    <div>
+                        <h5><?php echo $founders_title; ?></h5>
+                        <hr>
+                        <div><?php echo $founder_names; ?></div>
+                    </div>
+                </div>
 
-        <div class="container about-office">
-            <div>
-                <h5><?php echo $office_info_title; ?></h5>
-                <hr>
-                <div><?php echo $office_address; ?></div>
-            </div>
-        </div>
+                <div class="row about-past-show-chairs">
+                    <div>
+                        <h5><?php echo $past_show_chairs_title; ?></h5>
+                        <hr>
+                        <div><?php echo $past_show_chairs_names; ?></div>
+                    </div>
+                </div>
+
+                <div class="row about-staff">
+                    <div>
+                        <h5><?php echo $staff_title; ?></h5>
+                        <hr>
+                        <div><?php echo $staff_info; ?></div>
+                    </div>
+                </div>
+
+                <div class="row about-office">
+                    <div>
+                        <h5><?php echo $office_info_title; ?></h5>
+                        <hr>
+                        <div><?php echo $office_address; ?></div>
+                    </div>
+                </div>
+
+        </div><!--/main container-->
     </section>
 
 
